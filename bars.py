d5c4eb2d0cf3b3ec7b7a18035cfd8e78425fc48b
nvo87 <nvo87@yandex.ru>
2018-06-12 16:28:54 +0300

    functions code added

--- bars.py
+++ bars.py
@@ -1,21 +1,33 @@
 import json
+import io
 
 
 def load_data(filepath):
-    pass
+    with io.open(filepath, encoding='utf-8') as f_obj:
+        return json.load(f_obj)
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(bars_list):
+    return max(bars_list, key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(bars_list):
+    return min(bars_list, key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(bars_list, longitude, latitude):
+    return min(bars_list, key=lambda x: (x['geometry']['coordinates'][0] - longitude) ** 2 - \
+                                        (x['geometry']['coordinates'][1] - latitude) ** 2)
 
 
 if __name__ == '__main__':
-    pass
+    bars_filepath = 'bars.json'
+    bars = load_data(bars_filepath)
+    bars_list = bars['features']
+
+    longitude = float(input('Input your longitude:'))
+    latitude = float(input('Input your latitude:'))
+
+    print('The biggest bar -', get_biggest_bar(bars_list)['properties']['Attributes']['Name'])
+    print('The smallest one -', get_smallest_bar(bars_list)['properties']['Attributes']['Name'])
+    print('The closest one -', get_closest_bar(bars_list, longitude, latitude)['properties']['Attributes']['Name'])
