9fb1c136f2c739f1f55db3aaeb209a34bb4c78e4
nvo87 <nvo87@yandex.ru>
2018-06-13 11:56:53 +0300

    Remarks from melevir has been corrected: 1. io.open changed to open 2. too long lines are separated 3.exceptions are added 4. repeated actions changed to common functions

--- bars.py
+++ bars.py
@@ -1,33 +1,62 @@
 import json
-import io
 
 
 def load_data(filepath):
-    with io.open(filepath, encoding='utf-8') as f_obj:
-        return json.load(f_obj)
+    try:
+        with open(filepath, encoding='utf-8') as file_object:
+            return json.load(file_object)
+    except FileNotFoundError:
+        print('File bars.json not found or it has wrong name')
+        exit()
+    except json.decoder.JSONDecodeError:
+        print('File has invalid json structure')
+        exit()
+    except Exception as err:
+        print('Error is:', str(err))
+        exit()
 
 
-def get_biggest_bar(bars_list):
-    return max(bars_list, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+def get_biggest_bar(bars_list) -> dict:
+    return max(bars_list, key=lambda x: get_seats_count(x))
 
 
-def get_smallest_bar(bars_list):
-    return min(bars_list, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+def get_smallest_bar(bars_list) -> dict:
+    return min(bars_list, key=lambda x: get_seats_count(x))
 
 
-def get_closest_bar(bars_list, longitude, latitude):
-    return min(bars_list, key=lambda x: (x['geometry']['coordinates'][0] - longitude) ** 2 - \
-                                        (x['geometry']['coordinates'][1] - latitude) ** 2)
+def get_closest_bar(bars_list, longitude, latitude) -> dict:
+    return min(bars_list, key=lambda x: get_distance_to_bar(x, longitude, latitude))
+
+
+def input_coordinate(message='') -> float:
+    while True:
+        try:
+            return float(input(message))
+        except ValueError:
+            print('Your input has to be a number')
+
+
+def get_seats_count(bar: dict) -> str:
+    return bar['properties']['Attributes']['SeatsCount']
+
+
+def get_distance_to_bar(bar, your_longitude, your_latitude) -> str:
+    bar_longitude = bar['geometry']['coordinates'][0]
+    bar_latitude = bar['geometry']['coordinates'][1]
+    return (bar_longitude - your_longitude) ** 2 + (bar_latitude - your_latitude) ** 2
+
+
+def print_bar_name(bar_dict, message=''):
+    print(message, bar_dict['properties']['Attributes']['Name'])
 
 
 if __name__ == '__main__':
     bars_filepath = 'bars.json'
-    bars = load_data(bars_filepath)
-    bars_list = bars['features']
+    bars = load_data(bars_filepath)['features']
 
-    longitude = float(input('Input your longitude:'))
-    latitude = float(input('Input your latitude:'))
+    longitude = input_coordinate('Input your longitude:')
+    latitude = input_coordinate('Input your latitude:')
 
-    print('The biggest bar -', get_biggest_bar(bars_list)['properties']['Attributes']['Name'])
-    print('The smallest one -', get_smallest_bar(bars_list)['properties']['Attributes']['Name'])
-    print('The closest one -', get_closest_bar(bars_list, longitude, latitude)['properties']['Attributes']['Name'])
+    print_bar_name(get_biggest_bar(bars), 'The biggest bar -')
+    print_bar_name(get_smallest_bar(bars), 'The smallest one -')
+    print_bar_name(get_closest_bar(bars, longitude, latitude), 'The closest one -')
